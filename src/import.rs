99f841e05d6d6c20652c38ec352a259c8a52bae1
Josh Leverette <coder543@gmail.com>
2017-12-29 21:45:20 -0500

    support importing from a directory

--- src/import.rs
+++ src/import.rs
@@ -1,3 +1,5 @@
+use std::fs::read_dir;
+use std::path::{Path, PathBuf};
 use whatlang::detect;
 use database::Page;
 use std::fs::File;
@@ -11,10 +13,10 @@ use database::Database;
 use commoncrawl::{GetWetRef, WetRef};
 use helpers::ReadableDuration;
 
-fn load_source(source: String) -> Result<Vec<(String, Page)>, StrError> {
+fn load_source(source: PathBuf) -> Result<Vec<(String, Page)>, StrError> {
     let mut raw_pages = Vec::new();
 
-    // shorten peak memory usage time by deallocating content after this block
+    // shorten peak memory usage time by deallocating `content` after this block
     {
         let mut file = File::open(source)?;
         let content = &mut Vec::new();
@@ -49,12 +51,41 @@ fn load_source(source: String) -> Result<Vec<(String, Page)>, StrError> {
     Ok(pages)
 }
 
+fn path_to_files(path: String) -> Vec<PathBuf> {
+    let path = Path::new(&path);
+    if path.is_file() {
+        return vec![path.to_owned()];
+    }
+
+    let mut files = Vec::new();
+    match read_dir(path) {
+        Ok(dir) => for maybe_entry in dir {
+            for entry in maybe_entry {
+                let entry = entry.path();
+                if entry.is_file() && entry.extension().map_or(false, |ext| ext == "wet") {
+                    files.push(entry.to_owned());
+                }
+            }
+        },
+        _ => {}
+    }
+
+    files
+}
+
 impl Database {
     pub fn import(&mut self, sources: Vec<String>) -> Result<(), StrError> {
         let starting_page_count = self.len();
 
         let now = Instant::now();
-        let results = sources.into_par_iter().map(load_source).collect::<Vec<_>>();
+
+        let results = sources
+            .into_par_iter()
+            .flat_map(path_to_files)
+            .into_par_iter()
+            .map(load_source)
+            .collect::<Vec<_>>();
+
         let elapsed = now.elapsed().readable();
 
         for result in results {
