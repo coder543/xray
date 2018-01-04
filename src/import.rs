ba0a66dcd11cdaea42a440dd306586329b668129
Josh Leverette <coder543@gmail.com>
2018-01-03 22:02:02 -0500

    error handling improvement

--- src/import.rs
+++ src/import.rs
@@ -23,11 +23,16 @@ fn load_source(source: PathBuf) -> Result<Vec<(String, Page)>, StrError> {
     {
         let is_gzip = source.to_str().unwrap().ends_with(".gz");
 
-        let mut file = File::open(source)?;
+        let mut file = File::open(&source)?;
         let content = &mut Vec::new();
 
         if is_gzip {
-            MultiGzDecoder::new(BufReader::new(file)).read_to_end(content)?;
+            if let Err(_) = MultiGzDecoder::new(BufReader::new(file)).read_to_end(content) {
+                return Err(format!(
+                    "decoding gzip stream failed for {}",
+                    source.display()
+                ))?;
+            }
         } else {
             file.read_to_end(content)?;
         }
@@ -145,7 +150,6 @@ impl Database {
                     .into_par_iter()
                     .cloned()
                     .map(load_source)
-                    .map(Result::unwrap)
                     .collect::<Vec<_>>();
 
                 println!("sources loaded, now importing into database");
@@ -153,13 +157,20 @@ impl Database {
                 // sequential segment, generate URL IDs then persist the URL database
                 let mut results = results
                     .into_iter()
-                    .map(|pages| {
+                    .filter_map(|pages| {
+                        let pages = match pages {
+                            Ok(pages) => pages,
+                            Err(err) => {
+                                eprintln!("Error: {}", err.0);
+                                return None;
+                            }
+                        };
                         let mut pages = pages
                             .into_iter()
                             .map(|(url, page)| (self.insert_url(url), page))
                             .collect::<Vec<_>>();
                         pages.shrink_to_fit();
-                        pages
+                        Some(pages)
                     })
                     .collect::<Vec<_>>();
                 results.shrink_to_fit();
